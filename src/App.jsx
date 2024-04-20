e8f80ce25908f372ef089234e1a26852073226d2
Oyewunmi Oluwaseyi <ghost@Diana-2.local>
2024-04-20 02:23:24 +0100

    🎉 Project Started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LaundryService from './Componenets/Body/LaundryService'
+import Header from './Componenets/Header/Header'
+
+function App() {
+
+  return (
+   <>
+   <Header/>
+   <LaundryService/>
+   </>
+  )
+}
+
+export default App
---
+++ src/Componenets/styles.js
@@ -0,0 +1,44 @@
+// import 'launly/wp-content/plugins/elementor/assets/css/frontend-lite.min78fb.css'
+// import 'launly/wp-content/plugins/elementor/assets/css/widget-icon-box.min.css'
+// import 'launly/wp-content/plugins/elementor/assets/css/widget-icon-list.min.css'
+// import 'launly/wp-content/plugins/elementor/assets/lib/animations/animations.min78fb.css'
+// import 'launly/wp-content/plugins/elementor/assets/lib/font-awesome/css/all.min78fb.css'
+// import 'launly/wp-content/plugins/elementor/assets/lib/font-awesome/css/v4-shims.min78fb.css'
+// import 'launly/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min94a4.css'
+// import 'launly/wp-content/plugins/elementskit-lite/modules/elementskit-icon-pack/assets/css/ekiticons7c45.css'
+// import 'launly/wp-content/plugins/elementskit-lite/widgets/init/assets/css/responsive7c45.css'
+// import 'launly/wp-content/plugins/elementskit-lite/widgets/init/assets/css/widget-styles7c45.css'
+// import 'launly/wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor88a1.css'
+// import 'launly/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend88a1.css'
+// import 'launly/wp-content/plugins/jeg-elementor-kit/assets/css/elements/main6fca.css'
+// import 'launly/wp-content/plugins/jeg-elementor-kit/assets/css/elements/main6fca.css'
+// import 'launly/wp-content/plugins/jeg-elementor-kit/lib/jeg-framework/assets/css/jeg-dynamic-styles6f3e.css'
+// import 'launly/wp-content/plugins/metform/public/assets/css/metform-ui3b71.css'
+// import 'launly/wp-content/plugins/metform/public/assets/css/style3b71.css'
+// import 'launly/wp-content/plugins/metform/public/assets/css/text-editor3b71.css'
+// import 'launly/wp-content/plugins/template-kit-export/public/assets/css/template-kit-export-public.min365c.css'
+// import 'launly/wp-content/themes/hello-elementor/style.min1102.css'
+// import 'launly/wp-content/themes/hello-elementor/theme.min1102.css'
+
+import '../../wp-content/plugins/elementor/assets/css/frontend-lite.min78fb.css'
+import "../../wp-content/plugins/elementor/assets/css/widget-icon-box.min.css";
+import "../../wp-content/plugins/elementor/assets/css/widget-icon-list.min.css";
+import "../../wp-content/plugins/elementor/assets/lib/animations/animations.min78fb.css";
+import "../../wp-content/plugins/elementor/assets/lib/font-awesome/css/all.min78fb.css";
+import "../../wp-content/plugins/elementor/assets/lib/font-awesome/css/v4-shims.min78fb.css";
+import "../../wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min94a4.css";
+import "../../wp-content/plugins/elementskit-lite/modules/elementskit-icon-pack/assets/css/ekiticons7c45.css";
+import "../../wp-content/plugins/elementskit-lite/widgets/init/assets/css/responsive7c45.css";
+import "../../wp-content/plugins/elementskit-lite/widgets/init/assets/css/widget-styles7c45.css";
+import "../../wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor88a1.css";
+import "../../wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend88a1.css";
+import "../../wp-content/plugins/jeg-elementor-kit/assets/css/elements/main6fca.css?ver=2.6.3";
+import "../../wp-content/plugins/jeg-elementor-kit/lib/jeg-framework/assets/css/jeg-dynamic-styles6f3e.css";
+import "../../wp-content/plugins/metform/public/assets/css/metform-ui3b71.css";
+import "../../wp-content/plugins/metform/public/assets/css/style3b71.css";
+import "../../wp-content/plugins/metform/public/assets/css/text-editor3b71.css";
+import "../../wp-content/plugins/template-kit-export/public/assets/css/template-kit-export-public.min365c.css";
+import "../../wp-content/themes/hello-elementor/style.min1102.css";
+import "../../wp-content/themes/hello-elementor/theme.min1102.css";
+import "../../wp-includes/css/dist/block-library/style.min5352.css"
+
